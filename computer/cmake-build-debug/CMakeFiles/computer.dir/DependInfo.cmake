078a67e2a8ab41722341a0453c05ded353983ea7
kopter1989 <95441820+kopter1989@users.noreply.github.com>
2021-12-07 23:03:29 +0400

    Add files via upload

---
+++ computer/cmake-build-debug/CMakeFiles/computer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/MyProjects/Clion/computer/cpu.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/cpu.cpp.obj"
+  "C:/MyProjects/Clion/computer/disk.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/disk.cpp.obj"
+  "C:/MyProjects/Clion/computer/gpu.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/gpu.cpp.obj"
+  "C:/MyProjects/Clion/computer/kbd.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/kbd.cpp.obj"
+  "C:/MyProjects/Clion/computer/main.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/main.cpp.obj"
+  "C:/MyProjects/Clion/computer/ram.cpp" "C:/MyProjects/Clion/computer/cmake-build-debug/CMakeFiles/computer.dir/ram.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ computer/cmake-build-debug/CMakeFiles/computer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computer.dir/cpu.cpp.obj"
+  "CMakeFiles/computer.dir/disk.cpp.obj"
+  "CMakeFiles/computer.dir/gpu.cpp.obj"
+  "CMakeFiles/computer.dir/kbd.cpp.obj"
+  "CMakeFiles/computer.dir/main.cpp.obj"
+  "CMakeFiles/computer.dir/ram.cpp.obj"
+  "computer.exe"
+  "computer.exe.manifest"
+  "computer.lib"
+  "computer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
